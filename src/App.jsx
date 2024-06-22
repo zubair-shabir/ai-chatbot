8de4037ce0bc81315769439fd51e176828d46d33
zubair-shabir <zubairshabir980@gmail.com>
2024-06-22 15:50:51 +0530

    project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import ChatBot from "./ChatBot";
+
+function App() {
+  return (
+    <>
+      {/* <h1 className="bg-gray-800 text-white text-2xl">ChatBot</h1> */}
+     
+    </>
+  );
+}
+
+export default App;
